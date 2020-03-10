0a0fea2ef1b960f2f04c5b76168f9b025a1a332f
Cole320 <44954117+Cole320@users.noreply.github.com>
2020-03-10 09:38:40 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
-
+    print("Discord Desktop Control System Initialized")
 @client.event
 async def on_message(message):
     if message.author != client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith('!help'):
+        await message.channel.send('Included here are a list of all available commands:\n**!viewscreen**: Sends the user a screenshot of their current desktop')
 
 
 
