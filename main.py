686b18b463c2623b8ac59a1d186b5602ad901581
Cole320 <44954117+Cole320@users.noreply.github.com>
2020-03-10 09:27:15 -0400

    Created main.py
    The main python file for controlling and interfacing with the desktop.

---
+++ main.py
@@ -0,0 +1,22 @@
+import discord
+import pyscreenshot as ImageGrab
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+
+
+
+client.run('super secret big boy tokenn')
