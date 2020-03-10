07f8209ff1afd9e4ddc12a61d166fdc47b3a2c56
Cole320 <44954117+Cole320@users.noreply.github.com>
2020-03-10 13:58:09 -0400

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,1 +1,21 @@
+import subprocess
+import discord
 
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    print("Discord SSH dSystem Initialized")
+    
+@client.event
+async def on_message(message):
+    if message.author != client.user:
+        return
+
+    if message.content.startswith('!cmd'):
+        await message.channel.send('Please ')
+
+
+
+
+
+client.run('super secret big boy tokenn')
